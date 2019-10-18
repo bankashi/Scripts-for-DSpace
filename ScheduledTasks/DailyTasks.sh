7244a09637a85679e6b0991f59e4b186ce8da749
bankashi <trinigun.bankashi@gmail.com>
2019-10-18 10:38:17 -0500

    Fix "requests denied" and clean logs

--- ScheduledTasks/DailyTasks.sh
+++ ScheduledTasks/DailyTasks.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+#           _.====.._
+#         ,:._       ~-_
+#             `\        ~-_
+#               | _  _  |  `.
+#             ,/ /_)/ | |    ~-_
+#    -..__..-''  \_ \_\ `_      ~~--..__...----... BigWave ...
 #
 #Scheduled Tasks DSpaces - DAILY TASKS
 #
@@ -10,7 +16,9 @@ echo "This script must be run as root!"
 else
 ########################## Modules #########################
 DT_Permissions(){
-	chmod -R 775 /dspace/log/ && chmod -R 775 /dspace/var/oai/      
+	chmod -R 775 /dspace/log/ && chmod -R 775 /dspace/var/oai/ 
+	rm -rf /dspace/log/*
+	rm -rf /dspace/var/oai/requests/*
 }
 DT_Daily(){
 	cd /home/dspace
@@ -21,15 +29,21 @@ DT_Daily(){
 	/dspace/bin/dspace curate -q admin_ui
 	/dspace/bin/dspace sub-daily      
 }
+DT_Test(){
+cat > /home/dspace/prueba_$(date +"%Y-%m-%d").txt << "EOF"
+Esto es una prueba para saber si el user dspace funciona
+EOF
+}
 ########################## Exports #########################
 export -f DT_Permissions
 export -f DT_Daily
+export -f DT_Test
 ############################################################################################
 	if getent passwd | grep -c '^dspace:' > /dev/null 2>&1; then
 		cd /home/dspace
 		DT_Permissions
 		su dspace -c "bash -c DT_Daily"
-		DT_Permissions
+		chmod -R 775 /dspace/log/ && chmod -R 775 /dspace/var/oai/
 	else
 		echo "User doesn't exist...."
 	fi
